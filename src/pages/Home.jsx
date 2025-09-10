d742a28e3520e0667fba6f07a4001bac1abac65d
Thanuja Randima <thanujaeuw@gmail.com>
2025-09-10 13:26:31 +0530

    Navbar added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import { StarBackground } from "@/components/StarBackground";
+import { Navbar } from "@/components/Navbar.jsx";
 
 const Home = () => {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
         <StarBackground />
+        <Navbar />
     </div>
   );
 };
